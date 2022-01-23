7497861dcf9b1d574652b621b4e691119adaafa2
HampusEriksson <hampuse1@gmail.com>
2022-01-23 20:08:20 +0100

    Docstrings

--- Classes/ClassMenu.py
+++ Classes/ClassMenu.py
@@ -1,12 +1,14 @@
 class Pokemon:
-
     def __init__(self, name, type, hp):
+        """Each Pokémon has a name, a type and hp"""
         self.name = name
         self.type = type
         self.hp = hp
 
     def __str__(self):
+        """The str method returns the name of the Pokémon"""
         return self.name
+
 
 all_pokemon = []
 all_pokemon
@@ -14,14 +16,14 @@ all_pokemon
 while True:
     choice = input("1. Create X\n2. Look at all X\n3. Quit\n").lower()
     if choice in ["1", "create"]:
-        #Create a new X
+        # Create a new X
         pass
 
-    elif choice in ["2" , "look"]:
+    elif choice in ["2", "look"]:
         # Look at all X
         pass
 
-    elif choice in ["3" , "quit"]:
+    elif choice in ["3", "quit"]:
         break
 
     else:
