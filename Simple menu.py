ccc31db86b59f26a84ff0f9fab7c70c2e762efb8
Hampuse1 <hampuse1@hotmail.com>
2022-01-26 16:15:58 +0100

    Simple menu

--- Simple menu.py
+++ Simple menu.py
@@ -1,7 +1,6 @@
 # Simple menu with if
 while True:
     choice = input("1. Create X\n2. Look at all X\n3. Quit\n").lower()
-    print(choice in ["1", "create"])
     if choice in ["1", "create"]:
         #Create a new X
         pass
