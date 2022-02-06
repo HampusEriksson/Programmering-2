356ab9c18e4d925cbd5aa8600457c71928f7b0b0
HampusEriksson <hampuse1@gmail.com>
2022-02-06 20:00:13 +0100

    Quiz added

---
+++ Classes/Quiz.py
@@ -0,0 +1,6 @@
+class Question:
+
+    def __init__(self, question, correct_answer, wrong_answers):
+        self.question = question
+        self.correct_answer = correct_answer
+        self.wrong_answers = wrong_answers
