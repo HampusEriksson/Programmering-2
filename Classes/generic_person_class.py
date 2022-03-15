677bc7257465c2cb88e5c590cb2764c04625ac27
Hampuse1 <hampuse1@hotmail.com>
2022-03-15 10:21:50 +0100

    Generic classes

--- Classes/generic_person_class.py
+++ Classes/generic_person_class.py
@@ -5,6 +5,8 @@ class Person:
         self.year = year
         self.month = month
 
+    def __str__(self):
+        return f"Name: {self.name} Year: {self.year}"
     def __lt__(self, other):
         if isinstance(other, int):
             return self.year < other
@@ -52,8 +54,29 @@ class Person:
 
     def __ne__(self,other):
         return not self == other
+
+    def __add__(self, other):
+        if isinstance(other, Person):
+            return Person(self.name[:int(len(self.name)/2)]+other.name[int(len(self.name)/2):], int((self.year+other.year)/2),int((self.month+other.month)/2) )
+
+        elif isinstance(other, int):
+            return Person(self.name, self.year+other, self.month)
+
+    def __sub__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+# /
+    def __truediv__(self, other):
+        pass
+
+# //
+    def __floordiv__(self, other):
+        pass
 p1 = Person("Mary", 2004, 4)
 p2 = Person("Ida", 2004, 9)
 p3 = Person("Fabian", 2004, 6)
 
-print(p1 <= p3)
+print(p1 - p3)
---
+++ testing.py
@@ -0,0 +1,7 @@
+print(3+5)
+
+print("Hello" + "World")
+
+print(["Fabian"] + ["Godson"])
+
+print(("Fabian", "David") + ("Godson", "Jorge"))
