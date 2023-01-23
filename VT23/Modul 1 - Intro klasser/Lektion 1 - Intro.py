de8c14dc973fab4e6b9606fa687d7d3cc4faa16a
HampusEriksson <hampuse1@gmail.com>
2023-01-23 13:10:05 +0100

    Lektion 1 done

--- VT23/Modul 1 - Intro klasser/Lektion 1 - Intro.py
+++ VT23/Modul 1 - Intro klasser/Lektion 1 - Intro.py
@@ -2,9 +2,33 @@ from dataclasses import dataclass
 
 
 @dataclass
-class ClassName:
+class Enemy:
+
+    attack: int
+    hp: int
+    maxhp: int
+    charisma: int
+    name: str
+
     def __str__(self) -> str:
-        pass
+        return f"{self.name} - HP : {self.hp}."
+
+    def heal(self, amount):
+        if self.hp + amount > self.maxhp:
+            self.hp = self.maxhp
+        else:
+            self.hp += amount
+
+
+# Vi skapar ett objekt / en instans av klassen Enemy
+enemy1 = Enemy(20, 120, 120, 30, "Oskar Åberg")
+enemy2 = Enemy(hp=90, maxhp=120, charisma=50, name="Alex Jonsson", attack=20)
+
+print(f"{enemy1.name} attacks {enemy2.name}.")
+enemy2.hp -= enemy1.attack
+print(f"{enemy2.name} now has {enemy2.hp} hp left. Fs in the chat.")
+
+print(enemy1)
 
 
 class ClassName:
