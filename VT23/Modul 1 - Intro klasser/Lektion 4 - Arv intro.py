bbc98eb3aff2fa2bed6e29f88b13026fc5e75bad
Hampus Eriksson <hampus.eriksson@ga.ntig.se>
2023-01-29 16:18:52 +0100

    Lektion 4 prepp

---
+++ VT23/Modul 1 - Intro klasser/Lektion 4 - Arv intro.py
@@ -0,0 +1,22 @@
+class Dog:
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+
+class Cat:
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+
+class Rabbit:
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        pass
