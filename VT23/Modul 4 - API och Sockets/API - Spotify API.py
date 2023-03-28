a0c03949fe91bb2c99cba6cb879292ae49ff4985
HampusEriksson <hampuse1@gmail.com>
2023-03-28 15:02:40 +0200

    API prepp

--- VT23/Modul 4 - API och Sockets/API - Spotify API.py
+++ VT23/Modul 4 - API och Sockets/API - Spotify API.py
@@ -1,22 +1,32 @@
 import requests
 from SECRET_STUFF import *
-import base64
+import base64, json
 
 # https://developer.spotify.com/documentation/web-api
 # läs under "Getting started" för att få client_id och client_Secret
 
 
 def get_token():
-    auth_string = CLIENT_ID + ":" + CLIENT_SECRET
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+    AUTH_URL = "https://accounts.spotify.com/api/token"
 
-    url = "httpps://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic" + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
+    # POST
+    auth_response = requests.post(
+        AUTH_URL,
+        {
+            "grant_type": "client_credentials",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        },
+    )
 
-    data = {"grant_type": "client_credentials"}
-    result = requests.post(url, headerrs=headers, data=data)
-    json_result = json.loads(result)
+    # convert the response to JSON
+    auth_response_data = auth_response.json()
+
+    # save the access token
+    access_token = auth_response_data["access_token"]
+
+    return access_token
+
+
+token = get_token()
+print(token)
